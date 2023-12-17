3dd3fd9fa68e846a6c3dd8827d33155ead4a8d90
BryanRJR <bryanrantung98@gmail.com>
2023-12-17 21:26:57 +0700

    update email after and checkout

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -100,9 +100,4 @@ class CheckoutController extends Controller
     {
         return view('checkout.success');
     }
-
-    public function invoice(Checkout $checkout)
-    {
-        return $checkout;
-    }
 }
